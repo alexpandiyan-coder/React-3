0c0f6d4963fc0e5073b00360d126c359f9cc37ac
shivanleela <shivanleela5@gmail.com>
2024-09-27 10:46:32 +0530

    Update UserTable.js
    update table

--- src/Components/UserTable.js
+++ src/Components/UserTable.js
@@ -4,32 +4,17 @@ import Table from 'react-bootstrap/Table';
 function UserTable({tableData}) {
 
 <<<<<<< HEAD
-     const [data,setdata]= useState()
-
-     
-
-         const RefValue  =useRef()
-    
-  
-         console.log("useREf")
 
 =======
 >>>>>>> a39c010a7ff5f83dee2c461cb7d1ac7d7f687ea7
   return (
     <Table striped bordered hover variant="dark" className='w-50 m-auto'>
-      <thead>
         <tr>
 <<<<<<< HEAD
         <th>Name</th>
         <th>Password</th>
         </tr>      
-    
-     </table>
 =======
-          <th>Name</th>
-          <th>Password</th>
-        </tr>
-      </thead>
       <tbody>
        {
     tableData.map((item)=>{
