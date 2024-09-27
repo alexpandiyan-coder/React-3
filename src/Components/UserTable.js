a1674008e9f99ec30ba20981c23f6d68cdb5364b
alexpandiyan-cod <alexpandiyan.uniq3@gmail.com>
2024-09-27 11:21:51 +0530

    add some new line

--- src/Components/UserTable.js
+++ src/Components/UserTable.js
@@ -3,18 +3,13 @@ import Table from 'react-bootstrap/Table';
 
 function UserTable({tableData}) {
 
-<<<<<<< HEAD
 
-=======
->>>>>>> a39c010a7ff5f83dee2c461cb7d1ac7d7f687ea7
   return (
     <Table striped bordered hover variant="dark" className='w-50 m-auto'>
         <tr>
-<<<<<<< HEAD
         <th>Name</th>
         <th>Password</th>
         </tr>      
-=======
       <tbody>
        {
     tableData.map((item)=>{
@@ -29,7 +24,6 @@ function UserTable({tableData}) {
    }
       </tbody>
     </Table>
->>>>>>> a39c010a7ff5f83dee2c461cb7d1ac7d7f687ea7
 
   )
 }
